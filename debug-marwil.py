45e0b4ae766572c4f7ed95da2e44c4f290b24530
Jun Gong <jungong@anyscale.com>
2022-01-10 10:40:06 -0800

    Add CQL run

--- debug-marwil.py
+++ debug-marwil.py
@@ -11,7 +11,7 @@ import wandb
 
 def main():
     # Defaults
-    config = {
+    marwil_config = {
         "env": "HalfCheetahBulletEnv-v0",
         "framework": "tf",
         # Use input produced by expert SAC algo.
@@ -32,12 +32,63 @@ def main():
         }
     }
 
+    cql_config = {
+        "env": "HalfCheetahBulletEnv-v0",
+        "framework": "tf",
+        # Use input produced by expert SAC algo.
+        "input": ["~/halfcheetah_expert_sac.zip"],
+        "actions_in_input_normalized": True,
+        "soft_horizon": False,
+        "horizon": 1000,
+        "Q_model": {
+            "fcnet_activation": "relu",
+            "fcnet_hiddens": [256, 256, 256],
+        },
+        "policy_model": {
+            "fcnet_activation": "relu",
+            "fcnet_hiddens": [256, 256, 256],
+        },
+        "tau": 0.005,
+        "target_entropy": "auto",
+        "no_done_at_end": False,
+        "n_step": 3,
+        "rollout_fragment_length": 1,
+        "prioritized_replay": False,
+        "train_batch_size": 256,
+        "target_network_update_freq": 0,
+        "timesteps_per_iteration": 1000,
+        "learning_starts": 256,
+        "optimization": {
+            "actor_learning_rate": 0.0001,
+            "critic_learning_rate": 0.0003,
+            "entropy_learning_rate": 0.0001,
+        },
+        "num_workers": 0,
+        "num_gpus": 0,
+        "metrics_smoothing_episodes": 5,
+
+        # CQL Configs
+        "min_q_weight": 5.0,
+        "bc_iters": 20000,
+        "temperature": 1.0,
+        "num_actions": 10,
+        "lagrangian": False,
+
+        # Switch on online evaluation.
+        "always_attach_evaluation_results": True,
+        "evaluation_interval": 5,
+        "evaluation_config": {
+            "input": "sampler",
+        },
+        "always_attach_evaluation_results": True,
+    }
+
     ts = datetime.now().strftime('%m%d-%H%M%S')
     results = tune.run(
-        "BC",
-        name=f"debug-bc-{ts}",
+        "CQL",
+        name=f"debug-cql-{ts}",
         resume=None,
-        config=config,
+        config=cql_config,
         max_failures=5,
         num_samples=1,
         stop={"time_total_s": 3600 * 10},
